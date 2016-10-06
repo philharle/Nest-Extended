67465528aad4d6feab4a9792d3656e53be02f4e8
EC2 Default User <ec2-user@ip-172-31-56-223.ec2.internal>
2016-10-06 08:21:57 +0000

    Committing local changes

--- resources/config.php
+++ resources/config.php
@@ -1,18 +1,18 @@
 <?php
 // Your Nest username and password.
-define('USERNAME', 'USERNAME');
-define('PASSWORD', 'PASSWORD');
+define('USERNAME', 'usernameinhere');
+define('PASSWORD', 'passwordinhere');
 
 // The timezone you're in.
 // See http://php.net/manual/en/timezones.php for the possible values.
 $us_timezones = DateTimeZone::listIdentifiers(DateTimeZone::PER_COUNTRY, 'US');
-date_default_timezone_set('America/New_York');
+date_default_timezone_set('Europe/London');
 
 //Database settings
 $hostname='localhost';
-$username='USERNAME';
-$password='PASSWORD';
-$dbname='DATABASE';
+$username='usernameinhere';
+$password='passwordinhere';
+$dbname='matth41_nestdb';
 
 //WeatherUnderground API - Sign up at http://www.wunderground.com/weather/api/?apiref=c133a2be0b541640
 $wu_api_key = 'WeatherUnderground API Key';
